bd42c91137f26f34788446c1b7de31a302add648
Wayapay <wayapay@Wayapays-MacBook-Pro.local>
2023-02-14 21:01:27 +0300

    add the function

--- Python-challanges/MemoizedFibonacci.py
+++ Python-challanges/MemoizedFibonacci.py
@@ -1,2 +1,7 @@
 # Problem Context
 # The Fibonacci sequence is traditionally used to explain tree recursion.
+def fibonacci(n):
+    if n in [0, 1]:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
